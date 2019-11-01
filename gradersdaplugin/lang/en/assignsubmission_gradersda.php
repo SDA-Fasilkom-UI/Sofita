b79641bb79a3c12b4789aac6d3cc7bf5b3094dfd
Ragil Al Badrun Pasaribu <ragil.al@ui.ac.id>
2019-11-02 02:39:48 +0700

    initial moodle integration

---
+++ gradersdaplugin/lang/en/assignsubmission_gradersda.php
@@ -0,0 +1,14 @@
+<?php
+
+$string['default'] = 'Enabled by default';
+$string['default_help'] = 'If set, this submission method will be enabled by default for all new assignments.';
+$string['enabled'] = 'Grader SDA';
+$string['enabled_help'] = 'Grader SDA';
+$string['gradersda'] = 'Grader SDA';
+$string['memorylimit'] = 'Submission Memory Limit (MB)';
+$string['memorylimit_help'] = 'Submission Memory Limit (MB)';
+$string['pluginname'] = 'Grader SDA';
+$string['problemname'] = 'Problem Name';
+$string['problemname_help'] = 'Problem Name';
+$string['timelimit'] = 'Submission Time Limit (second)';
+$string['timelimit_help'] = 'Submission Time Limit (second)';
---
+++ gradersdaplugin/settings.php
@@ -0,0 +1,6 @@
+<?php
+
+// Note: This is on by default.
+$settings->add(new admin_setting_configcheckbox('assignsubmission_gradersda/default',
+                   new lang_string('default', 'assignsubmission_gradersda'),
+                   new lang_string('default_help', 'assignsubmission_gradersda'), 0));
