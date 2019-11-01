c5dfec66bfe3ba6e898f67aee1fb3a723f3cd7b3
ragil.al <ragil.al@ui.ac.id>
2019-11-01 16:46:16 +0700

    Done user admin

--- graderng/app/admin.py
+++ graderng/app/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User, Group
 
-# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'first_name', 'last_name', 'is_superuser')
+    list_filter = ('is_superuser',)
+    fieldsets = (
+        ('', {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'email')}),
+        ('Permissions', {'fields': ('is_active', 'is_superuser')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+
+    def save_model(self, request, user, form, change):
+        if request.user.is_superuser:
+            user.is_staff = user.is_superuser
+            user.save()
+
+
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
+admin.site.unregister(Group)
