90d0be954015477092fb6f3e444d0e103367e22b
tengkuizdihar <tengkuizdihar@gmail.com>
2019-11-01 17:27:49 +0700

    Create models for Submission file and Submission Token.
    * Token will generate its own secure random token upon creation.

--- graderng/app/models.py
+++ graderng/app/models.py
@@ -1,3 +1,20 @@
-from django.db import models
+from djongo import models
+import hashlib, secrets
 
 # Create your models here.
+class Submission(models.Model):
+    name = models.CharField(max_length=255)
+    problem_name = models.CharField(max_length=255)
+    assignment_id = models.IntegerField()
+    user_id = models.IntegerField()
+    content = models.TextField()
+    id_number = models.CharField(max_length=15)
+    time_limit = models.IntegerField(default=3)
+    memory_limit = models.IntegerField(default=256)
+
+def generate_token():
+    secure_string = secrets.token_urlsafe(69)
+    return hashlib.sha256(secure_string.encode())
+
+class Token(models.Model):
+    token = models.CharField(max_length=63, default=generate_token)
