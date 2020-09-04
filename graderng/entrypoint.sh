50dc362d94566b45284ed42132168b4963209c71
ragil.al <ragil.al@ui.ac.id>
2020-09-04 14:35:54 +0700

    Fix entrypoint.sh

--- graderng/entrypoint.sh
+++ graderng/entrypoint.sh
@@ -2,10 +2,10 @@
 
 if [ "$1" == "worker" ]; then
     celery -A graderng worker -l info --concurrency=$WORKER_CONCURRENCY --pool threads
-else if [ "$1" == "feedbacks_jobs" ]; then
+elif [ "$1" == "feedbacks_jobs" ]; then
     celery -A graderng worker -l info -Q feedbacks_jobs --concurrency=$FEEDBACKS_JOBS_CONCURRENCY --pool gevent
 else
     python3 manage.py collectstatic --noinput
     python3 manage.py migrate
-    gunicorn graderng.wsgi --worker-class gevent --bind 0.0.0.0:18080 --log-level info
+    gunicorn graderng.wsgi --worker-clas gevent --bind 0.0.0.0:18080 --log-level info
 fi
