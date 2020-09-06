d6207dd33798937dd95b3b3206f28fb306642218
ragil.al <ragil.al@ui.ac.id>
2020-09-06 16:52:14 +0700

    Fix media

--- graderng/entrypoint.sh
+++ graderng/entrypoint.sh
@@ -3,7 +3,7 @@
 if [ "$1" == "worker" ]; then
     celery -A graderng worker -l info --concurrency=$WORKER_CONCURRENCY
 elif [ "$1" == "testcases" ]; then
-    celery -A graderng worker -l error -Q testcases --concurrency=$TESTCASES_CONCURRENCY
+    celery -A graderng worker -l warn -Q testcases --concurrency=$TESTCASES_CONCURRENCY
 elif [ "$1" == "feedbacks_jobs" ]; then
     celery -A graderng worker -l info -Q feedbacks_jobs --concurrency=$FEEDBACKS_JOBS_CONCURRENCY --pool gevent
 else
