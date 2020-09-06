b4297c73d3d7b4bf9bc2d3c7693b0caf70c06e00
ragil.al <ragil.al@ui.ac.id>
2020-09-06 20:04:53 +0700

    Set redis maxmemory

--- graderng/app/__init__.py
+++ graderng/app/__init__.py
@@ -9,5 +9,11 @@ redis_connection_pool = redis.ConnectionPool(
     port=settings.REDIS_PORT,
     password=settings.REDIS_PASSWORD,
     db=1)
+redis_conn = redis.Redis(connection_pool=redis_connection_pool)
+
+# https://redis.io/topics/lru-cache
+redis_conn.config_set("maxmemory", "1gb")
+redis_conn.config_set("maxmemory-policy", "volatile-lru")
+
 
 disk_cache = diskcache.Cache("__cache")
