c6030e25e6ccf57f6e09ff3de723a0869d20dd51
ragil.al <ragil.al@ui.ac.id>
2020-09-06 16:44:30 +0700

    Fix Dockerfile final and change log level

--- graderng/entrypoint.sh
+++ graderng/entrypoint.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 if [ "$1" == "worker" ]; then
-    celery -A graderng worker -l error --concurrency=$WORKER_CONCURRENCY
+    celery -A graderng worker -l info --concurrency=$WORKER_CONCURRENCY
 elif [ "$1" == "testcases" ]; then
     celery -A graderng worker -l error -Q testcases --concurrency=$TESTCASES_CONCURRENCY
 elif [ "$1" == "feedbacks_jobs" ]; then
-    celery -A graderng worker -l error -Q feedbacks_jobs --concurrency=$FEEDBACKS_JOBS_CONCURRENCY --pool gevent
+    celery -A graderng worker -l info -Q feedbacks_jobs --concurrency=$FEEDBACKS_JOBS_CONCURRENCY --pool gevent
 else
     python3 manage.py collectstatic --noinput
     python3 manage.py migrate
-    gunicorn graderng.wsgi --worker-clas gevent --bind 0.0.0.0:18080 --log-level error
+    gunicorn graderng.wsgi --worker-clas gevent --bind 0.0.0.0:18080 --log-level info
 fi
