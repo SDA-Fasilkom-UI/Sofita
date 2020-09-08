236c6e230a0bafa7a2885ba1a541f8ec78bf46ce
ragil.al <ragil.al@ui.ac.id>
2020-09-08 11:42:48 +0700

    Prevent delete submission

--- graderng/grader/admin.py
+++ graderng/grader/admin.py
@@ -100,6 +100,9 @@ class SubmissionAdmin(admin.ModelAdmin):
     def has_add_permission(self, request, obj=None):
         return False
 
+    def has_delete_permission(self, request, obj=None):
+        return False
+
     def get_actions(self, request):
         actions = super().get_actions(request)
         if 'delete_selected' in actions:
