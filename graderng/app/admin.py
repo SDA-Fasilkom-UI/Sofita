093a88e60c51041df200d2861b96e0eb4a788288
ragil.al <ragil.al@ui.ac.id>
2020-09-08 11:35:36 +0700

    Change admin logic

--- graderng/app/admin.py
+++ graderng/app/admin.py
@@ -1,24 +1,13 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from django.contrib.auth.models import User, Group
+from django.contrib.auth.models import User
 
 from app.models import Token
 
 
 class CustomUserAdmin(UserAdmin):
-    list_display = ('username', 'first_name', 'last_name', 'is_superuser')
-    list_filter = ('is_superuser',)
-    fieldsets = (
-        ('', {'fields': ('username', 'password')}),
-        ('Personal info', {'fields': ('first_name', 'last_name', 'email')}),
-        ('Permissions', {'fields': ('is_active', 'is_superuser')}),
-        ('Important dates', {'fields': ('last_login', 'date_joined')}),
-    )
-
-    def save_model(self, request, user, form, change):
-        if request.user.is_superuser:
-            user.is_staff = user.is_superuser
-            user.save()
+    list_display = ('username', 'is_staff', 'is_superuser')
+    list_filter = ('is_staff', 'is_superuser',)
 
 
 class TokenAdmin(admin.ModelAdmin):
@@ -29,6 +18,5 @@ class TokenAdmin(admin.ModelAdmin):
 
 admin.site.unregister(User)
 admin.site.register(User, CustomUserAdmin)
-admin.site.unregister(Group)
 
 admin.site.register(Token, TokenAdmin)
